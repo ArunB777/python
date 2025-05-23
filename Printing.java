e7e1c90eea44f4cbd741c9af6c41948ccc5585fe
ArunB777 <168570617+ArunB777@users.noreply.github.com>
2025-05-23 12:13:08 +0530

    cleanup

--- Printing.java
+++
@@ -1,8 +0,0 @@
-public class Printing{
-    public static void main(String []args){
-        int x=10;
-        float y=3.45f;
-        String s="java";
-        System.out.printf("%20s",s);
-    }
-}
